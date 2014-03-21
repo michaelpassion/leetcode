17895907cfdd4138168d155019c2bd53c2aac580
Michael <michael@Michael.(none)>
2014-03-21 18:45:15 +0800

    位运算解决，nice

---
+++ single-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(int A[], int n) {
+        int single=0;
+        for(int i=0; i<n; i++)
+          single^=A[i];
+         
+        return single^0;
+    }
+
+};
