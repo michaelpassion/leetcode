ca364ae8dba267dfb7013201160ac2abb3f43172
Michael <michael@Michael.(none)>
2014-03-21 23:14:51 +0800

    this is easy

---
+++ same-tree.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+
+    bool isSameTree(TreeNode *p, TreeNode *q) {
+        if(p!=NULL && q!=NULL)
+        {
+            if(p->val != q->val)
+                 return false;
+            else
+            {
+               return isSameTree(p->right,q->right) &&  isSameTree(p->left,q->left);
+            }
+        }else if(p==NULL && q==NULL)
+                return true;
+          else return false;
+
+    }
+};
